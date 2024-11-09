19b5bb7a36ab736ab6ec1488485b12cdff6a5dd9
ayushhii16 <ayushhii16@gmail.com>
2024-11-09 19:35:48 +0100

    pip installation files added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Homework2Dsss
-Welcome to the second homework. With this activity: ● Setup a Git repository. ● Clean-up and improve the code provided. ● Make repository pip installable.
+Welcome to the second homework. With this activity: 
+    ● Setup a Git repository. 
+    ● Clean-up and improve the code provided. 
+    ● Make repository pip installable.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math-quiz",
+    version="0.1",
+    description="A simple math quiz game in Python",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Ayushi Jaimani",
+    author_email="ayushhii16@gmail.com",
+    url="https://github.com/ayushhii16/Homework2Dsss.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.6',
+    test_suite='tests', 
+)
